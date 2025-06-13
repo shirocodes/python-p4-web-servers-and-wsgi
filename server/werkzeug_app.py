d2587794b5b47f39d319b5953159b028d913b519
shirocodes <ishirapt@gmail.com>
2025-06-13 17:32:25 +0300

    Built server and application.

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+from werkzeug.wrappers import Request, Response
+
+# function is decorated with the Request.application method, which tells it to run any code inside of the function in the browser 
+# at the location we specify with our development server.
+@Request.application 
+def application(request):
+    print(f'This web server is running at {request.remote_addr}')
+    return Response('A WSGI generated this response!')
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple(
+        hostname='localhost',
+        port=5555,
+        application=application
+    )
